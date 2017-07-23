149c6ecd9581cd97c873c80c029e4bff7fa6c31f
Rahul Barua <rahul.ctg62@gmail.com>
2017-07-23 09:36:16 +0600

    Add files via upload

---
+++ Problem-26.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codingcontest12
+{
+    class Problem_26
+    {
+        public static void Main()
+        {
+            string s;
+            string output;
+            int test;
+
+            test = Convert.ToInt16(Console.ReadLine());
+            while (test > 0)
+            {
+                s = Console.ReadLine();
+                char[] carray = s.ToCharArray();
+                Array.Reverse(carray);
+
+                output = new string(carray);
+                Console.WriteLine(output);
+                test--;
+            }
+        }
+    }
+}
