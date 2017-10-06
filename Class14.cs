47bc816ecb511f9c744601ddfa153b4da20ac52a
Rahul Barua <rahul.ctg62@gmail.com>
2017-10-06 09:07:50 +0600

    Problem-14
    Problem-14

---
+++ Class14.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codingcontest12
+{
+    class Class14
+    {
+        static void Main(string[] args)
+        {
+
+
+            int test = Convert.ToInt32(Console.ReadLine());
+            double total = 0,carry=0;
+            string[] st = new string[3];
+            double[] a = new double[3];
+            while(test>0)
+            {
+                  string s = Console.ReadLine();
+
+
+
+             st = s.Split(' ');
+             for(int i=0;i<st.Length;i++)
+             {
+                 a[i] = Convert.ToDouble(st[i]);
+                 carry=Math.Pow(a[i], 2);
+                 total = total + carry;
+                 
+             }
+             total = Math.Sqrt(total);
+             //total = Math.Round(total, 2);
+             Console.WriteLine(total.ToString("0.00"));
+                 test--;
+                 total = 0; carry = 0;
+            }
+          
+           
+
+            Console.ReadKey();
+        }
+    }
+}
